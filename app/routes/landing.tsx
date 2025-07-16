4b1070ff86e088d7d5ede3ba87cab4b6b06747ce
Micha Buchholz <mail@lean-stack.rocks>
2025-07-16 23:52:05 +0200

    feat: Implement landing page.

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -1,20 +1,35 @@
-import type { Route } from "./+types/landing";
+import { BookmarkIcon, LightbulbIcon } from "lucide-react";
+import { Link } from "react-router";
 
-import { db } from "@/db/instance";
-
-export async function loader() {
-  const user = await db.query.users.findFirst();
-  return { user };
-}
-
-export default function LandingRoute({ loaderData }: Route.ComponentProps) {
+export default function LandingRoute() {
   return (
-    <div>
-      <title>les favoris</title>
-      <h1>
-        <small>les</small> favoris
+    <div className="grow mx-auto max-w-xl flex flex-col items-center justify-center gap-4 p-4 text-center ">
+      <title>Favoris</title>
+      <h1 className="flex items-center gap-4 text-accent-11 my-2">
+        <BookmarkIcon className="size-12" />
+        <span className="text-5xl">les favoris</span>
       </h1>
-      <h2>User: {loaderData.user?.name}</h2>
+      <p className="text-xl text-accent-12 text-balance">
+        Sammlung meiner Lesezeichen aus dem weltweiten Netz
+      </p>
+      <p className="text-app-11">
+        Früher gab es Lesezeichen-Dienste en masse. Naja, vielleicht etwas
+        übertrieben. Nach und nach wurde leider alles eingestellt.
+      </p>
+      <p className="flex max-md:flex-col justify-center items-center text-accent-12 gap-2">
+        <LightbulbIcon className="shrink-0 self-center" />
+        <span>
+          Dann schreibe mir halt meinen eigenen Dienst. Und hier ist er!
+        </span>
+      </p>
+      <div>
+        <Link
+          className="text-xl text-accent-11 underline underline-offset-2"
+          to="/links"
+        >
+          Zu den Links
+        </Link>
+      </div>
     </div>
   );
 }
