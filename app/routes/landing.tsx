0801ef01ccdb74733185d16c79921939d809124c
Micha Buchholz <mail@lean-stack.rocks>
2025-07-16 08:03:02 +0200

    chore: Loading user from neon db. Proof of concept

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -1,10 +1,17 @@
-export default function LandingRoute() {
+import type { Route } from "./+types/landing";
+
+export async function loader({ context }: Route.LoaderArgs) {
+  return await context.db.query.users.findFirst();
+}
+
+export default function LandingRoute({ loaderData }: Route.ComponentProps) {
   return (
     <div>
       <title>les favoris</title>
       <h1>
         <small>les</small> favoris
       </h1>
+      <h2>User: {loaderData?.name}</h2>
     </div>
   );
 }
